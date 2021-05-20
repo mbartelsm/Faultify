010d9bfaa26bf23190f6f6d06aa92dafb89f2fb8
Miguel Bartelsman <mbartelsmanm@gmail.com>
2021-05-20 19:18:09 +0200

    Fix bad extension handling by FileDuplication.cs

--- Faultify.TestRunner/ProjectDuplication/TestProjectDuplicator.cs
+++ Faultify.TestRunner/ProjectDuplication/TestProjectDuplicator.cs
@@ -80,7 +80,7 @@ namespace Faultify.TestRunner.ProjectDuplication
             }
 
             IEnumerable<FileDuplication>? initialCopies = testProject.ProjectReferences
-                .Select(x => new FileDuplication(_newDirInfo.FullName, Path.GetFileNameWithoutExtension(x) + ".dll"));
+                .Select(x => new FileDuplication(_newDirInfo.FullName, Path.GetFileName(x)));
             TestProjectDuplication? testProjectDuplication = new TestProjectDuplication(
                 new FileDuplication(_newDirInfo.FullName, Path.GetFileName(testProject.AssemblyPath)),
                 initialCopies,
@@ -118,7 +118,7 @@ namespace Faultify.TestRunner.ProjectDuplication
             IEnumerable<FileDuplication> duplicatedAssemblies = _projectInfo
                 .ProjectReferences
                 .Select(x =>
-                    new FileDuplication(duplicatedDirectoryPath, Path.GetFileNameWithoutExtension(x) + ".dll"));
+                    new FileDuplication(duplicatedDirectoryPath, Path.GetFileName(x)));
 
             return
                 new TestProjectDuplication(
