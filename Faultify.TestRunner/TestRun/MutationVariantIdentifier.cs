abb42bf7d7b5055e353c7c89459135e19ce3c11c
Miguel Bartelsman <mbartelsmanm@gmail.com>
2021-05-21 10:08:02 +0200

    Split MutationVariant.cs, Moved TestHostRunnerFactory.cs

---
+++ Faultify.TestRunner/TestRun/MutationVariantIdentifier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Faultify.TestRunner.TestRun
+{
+    /// <summary>
+    ///     Single mutation variant that serves as an identifier for a particular possible mutation.
+    /// </summary>
+    public readonly struct MutationVariantIdentifier
+    {
+        public MutationVariantIdentifier(
+            HashSet<string> testNames,
+            string memberName,
+            int mutationId,
+            int mutationGroupId
+        )
+        {
+            TestCoverage = testNames;
+            MemberName = memberName;
+            MutationId = mutationId;
+            MutationGroupId = mutationGroupId;
+        }
+
+        public int MutationId { get; }
+        public int MutationGroupId { get; }
+        public string MemberName { get; }
+        public HashSet<string> TestCoverage { get; }
+    }
+}
